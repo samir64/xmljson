bb752d077918974694f71636ae0cae8c47d55e48
samir64 <mail6410@gmail.com>
2020-06-30 20:14:16 +0430

    Update & Fix

--- XmlJson.php
+++ XmlJson.php
@@ -6,146 +6,187 @@
  */
 class XmlJson
 {
-    private $attributePrefix;
-    private $innerTextLable;
+  private $childrenLabel;
+  // private $attributePrefix;
+  // private $innerTextLable;
 
-    private function json(SimpleXMLElement $xmlElement)
-    {
-        $result = array();
+  private function clean_json(array $json, $prefix = "")
+  {
+    $result = array();
 
-        foreach ($xmlElement->children() as $child => $value) {
-            $result[$child][] = $this->json($value);
+    if (count($json) === 0) {
+      $result = null;
+    } else {
+      foreach ($json as $key => $value) {
+        if (is_array($value)) {
+          $clean_json = $this->clean_json($value, ($key === $this->childrenLabel) ? $key : "");
+        } else {
+          $clean_json = $value;
         }
 
-        foreach ($result as $key => $value) {
-            if (count($result[$key]) == 1) {
-                $result[$key] = $value[0];
-            }
-        }
-
-        foreach ($xmlElement->attributes() as $attrName => $attrValue) {
-            $result[$this->attributePrefix . $attrName] = (string)$attrValue;
-        }
-
-        $text = (string)trim(str_replace("\n", "", $xmlElement));
-        if (!empty($text)) {
-            if (count($result) == 0) {
-                $result = $text;
+        if (count($json) === 1) {
+          if (($key === $this->childrenLabel) || is_numeric($key)) {
+            $result = $clean_json;
+          } else {
+            $result[$prefix . $key] = $clean_json;
+          }
+        } else {
+          if ($key === $this->childrenLabel) {
+            if (count($value) === 1) {
+              $result[$key] = $clean_json;
             } else {
-                $result[$this->innerTextLable] = $text;
+              foreach ($clean_json as $k => $v) {
+                $result[$k] = $v;
+              }
             }
-        }
-
-        if (is_array($result)) {
-            if (count($result) == 0) {
-                $result = null;
+          } else {
+            if (!empty($prefix) && is_numeric($key)) {
+              $result[$prefix] = $clean_json;
             } else {
-                foreach ($result as $key => $value) {
-                    if (count($result[$key]) == 0) {
-                        $result[$key] = null;
-                    }
-                }
+              $result[$prefix . $key] = $clean_json;
             }
+          }
         }
-
-        return $result;
+      }
     }
 
-    private function xml(array $jsonItem, SimpleXMLElement $root, SimpleXmlElement $parent)
-    {
-        if (array_key_exists($this->innerTextLable, $jsonItem)) {
-            $root[] = $jsonItem[$this->innerTextLable];
+    return $result;
+  }
+
+  private function json(SimpleXMLElement $xmlElement)
+  {
+    $result = array(/* $this->childrenLabel => [] */);
+
+    foreach ($xmlElement->children() as $child => $value) {
+      $result[$this->childrenLabel][$child][] = $this->json($value);
+    }
+
+    foreach ($xmlElement->attributes() as $attrName => $attrValue) {
+      $result[$attrName] = (string) $attrValue;
+    }
+
+    $text = (string) trim(str_replace("\n", "", $xmlElement));
+    if (!empty($text)) {
+      $result[$this->childrenLabel][] = $text;
+    }
+
+    return $result;
+  }
+
+  private function xml(array $jsonItem, SimpleXMLElement $root, SimpleXmlElement $parent)
+  {
+    if (array_key_exists($this->innerTextLable, $jsonItem)) {
+      $root[] = $jsonItem[$this->innerTextLable];
+    }
+    foreach ($jsonItem as $key => $value) {
+      if (is_array($value)) {
+        // $LOGGER_VARIABLES = $key;
+        // LOGGER(__FILE__, __METHOD__, __LINE__, function () use ($LOGGER_VARIABLES) {
+        //     echo ($LOGGER_VARIABLES);
+        // });
+        if (is_numeric($key)) {
+          if ($key == 0) {
+            $this->xml($value, $root, $parent);
+          } else {
+            $child = $parent->addChild($root->getName());
+            $this->xml($value, $child, $parent);
+          }
+        } else {
+          $child = $root->addChild($key);
+          $this->xml($value, $child, $root);
         }
-        foreach ($jsonItem as $key => $value) {
-            if (is_array($value)) {
-                // $LOGGER_VARIABLES = $key;
-                // LOGGER(__FILE__, __METHOD__, __LINE__, function () use ($LOGGER_VARIABLES) {
-                //     echo ($LOGGER_VARIABLES);
-                // });
-                if (is_numeric($key)) {
-                    if ($key == 0) {
-                        $this->xml($value, $root, $parent);
-                    } else {
-                        $child = $parent->addChild($root->getName());
-                        $this->xml($value, $child, $parent);
-                    }
-                } else {
-                    $child = $root->addChild($key);
-                    $this->xml($value, $child, $root);
-                }
-            } elseif ($key != $this->innerTextLable) {
-                if (is_numeric($key)) {
-                    if ($key == 0) {
-                        $root[0] = $value;
-                    } else {
-                        $parent->addChild($root->getName(), $value);
-                    }
-                // } elseif ($key == $this->innerTextLable) {
-                //     $root[] = $value;
-                } elseif (substr($key, 0, strlen($this->attributePrefix)) == $this->attributePrefix) {
-                    $root->addAttribute(substr($key, strlen($this->attributePrefix)), $value);
-                } else {
-                    $root->addChild($key, $value);
-                }
-                // $LOGGER_VARIABLES = [$key => $value, "Result" => str_replace("<", "&lt;", $parent->asXML())];
-                // LOGGER(__FILE__, __METHOD__, __LINE__, function () use ($LOGGER_VARIABLES) {
-                //     var_dump($LOGGER_VARIABLES);
-                // });
-            }
+      } else {
+        if (is_numeric($key)) {
+          if ($key == 0) {
+            $root[0] = $value;
+          } else {
+            $parent->addChild($root->getName(), $value);
+          }
+          // } elseif ($key == $this->innerTextLable) {
+          //     $root[] = $value;
+        } elseif (strpos($key, $this->childrenLabel) === 0) {
+          if ($key === $this->childrenLabel) {
+            $root[] = $value;
+          } else {
+            $root->addChild(substr($key, strlen($this->childrenLabel)), $value);
+          }
+        } else {
+          $root->addAttribute($key, $value);
+          // $root->addAttribute(substr($key, strlen($this->childrenLabel)), $value);
         }
+        // $LOGGER_VARIABLES = [$key => $value, "Result" => str_replace("<", "&lt;", $parent->asXML())];
+        // LOGGER(__FILE__, __METHOD__, __LINE__, function () use ($LOGGER_VARIABLES) {
+        //     var_dump($LOGGER_VARIABLES);
+        // });
+      }
+    }
+  }
+
+  public function __construct($childrenLabel = "@")
+  {
+    $this->childrenLabel = $childrenLabel;
+    // $this->attributePrefix = $attributePrefix;
+    // $this->innerTextLable = $innerTextLable;
+  }
+
+  public function __get($name)
+  {
+    switch ($name) {
+      case "childrenLabel":
+        return $this->childrenLabel;
+        break;
+
+        // case 'attributePrefix':
+        //     return $this->attributePrefix;
+        //     break;
+
+        // case "innerTextLable":
+        //     return $this->innerTextLable;
+        //     break;
+
+      default:
+        # code...
+        break;
+    }
+  }
+
+  public function __set($name, $value)
+  {
+  }
+
+  /**
+   * @param array $json
+   * @param string $root = "root"
+   * @return SimpleXMLElement
+   */
+  public function toXml(array $json, $root = "root")
+  {
+    if (count($json) === 1) {
+      foreach ($json as $key => $value) {
+        $root = $key;
+        $json = $value;
+      }
     }
 
-    public function __construct($attributePrefix = "@", $innerTextLable = "#text")
-    {
-        $this->attributePrefix = $attributePrefix;
-        $this->innerTextLable = $innerTextLable;
+    $string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><$root></$root>";
+    $rootElement = new SimpleXMLElement($string);
+    $this->xml($json, $rootElement, $rootElement);
+
+    return $rootElement;
+  }
+
+  /**
+   * @param SimpleXMLElement $xml
+   * @return array
+   */
+  public function toJson(SimpleXMLElement $xml, $clean = false)
+  {
+    $result = $this->json($xml);
+
+    if ($clean) {
+      $result = $this->clean_json($result);
     }
 
-    public function __get($name)
-    {
-        switch ($name) {
-            case 'attributePrefix':
-                return $this->attributePrefix;
-                break;
-
-            case "innerTextLable":
-                return $this->innerTextLable;
-                break;
-
-            default:
-                # code...
-                break;
-        }
-    }
-
-    public function __set($name, $value)
-    {
-
-    }
-
-    /**
-     * @param array $json
-     * @param string $root
-     * @return SimpleXMLElement
-     */
-    public function toXml(array $json, $root = "root")
-    {
-        $string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><$root></$root>";
-
-        $rootElement = new SimpleXMLElement($string);
-
-        $this->xml($json, $rootElement, $rootElement);
-
-        return $rootElement;
-    }
-
-    /**
-     * @param SimpleXMLElement $xml
-     * @return array
-     */
-    public function toJson(SimpleXMLElement $xml)
-    {
-        return $this->json($xml);
-    }
+    return [$xml->getName() => $result];
+  }
 }
-
